afd0540c7041139120077bc7690921a33fd1fc07
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-22 22:43:26 +0000

    Client and play

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.223.133", // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+  })
+  //player's name
+  conn.on('connect', () => {
+    conn.write('Name: KCB');
+  });
+  // code that does something when the connection is first established
+  conn.on("data", (info) => {
+    console.log("Data received!")
+    console.log(info)
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+module.exports = { connect };
