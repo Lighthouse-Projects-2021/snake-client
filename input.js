8d31db4afe19def07fb5cff2c90ea300b446e033
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-23 00:22:48 +0000

    add input

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+const stdin = setupInput()
+stdin.on("data", handleUserInput)
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,12 @@
-const { connect } = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// setup interface to handle user input from stdin
+console.log("Connecting ...");
+connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
+setupInput();
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
 
-  //Dpenendo da tecla apertada vamos enviar uma request para o servidor
-  //para guiar a cobrinha 
-
-  console.log("recebi " + key)
-};
-
-const stdin = setupInput()
-stdin.on("data", handleUserInput);
 
 
 
